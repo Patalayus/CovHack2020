df0b4e83a181c8bc9f49d65d9494208e054907d0
Patalayus <Uiouiouio4>
2020-02-15 14:32:53 +0000

    initial commit

---
+++ src/farquest.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class farquest {
+
+    static void charCreation(String x){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What gender is "+x+"?");
+        String genChar = scan.nextLine();
+        if(genChar.contains("F")||genChar.contains("f")){
+            System.out.println(x+" is Female.. now to get to the story");
+        }else if(genChar.contains("M")||genChar.contains("m")) {
+            System.out.println(x + " is Male.. now to get to the story");
+        }else{
+            charCreation("Your characer is");
+        }
+    }
+
+    public static void main(String []args){
+        System.out.println("COVHACK 2020 PROJECT:\n" +
+                "" +
+                "|~~    /\\    |~~\\   /~~\\   |   |  |~~  /~~\\  ~~|~~\n" +
+                "|--   /__\\   |__/  |    |  |   |  |--  '--.    |  \n" +
+                "|    /    \\  |  \\   \\__X    \\_/   |__  \\__/    |  \n" +
+                "                                                  ");
+
+        System.out.println("by Patalayus");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your character name:\n");
+        String chaName = sc.nextLine();
+        System.out.println("Welcome "+chaName+"!\n" );
+        charCreation(chaName);
+
+    }
+
+}
