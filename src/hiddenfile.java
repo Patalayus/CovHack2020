daa361d9d655a0961e7fbf0b447f4a3ab7b6026f
Patalayus <lukecharles222@gmail.com>
2020-03-12 09:47:28 +0000

    added story

--- src/hiddenfile.java
+++ src/hiddenfile.java
@@ -9,15 +9,27 @@ public class hiddenfile{
         "1. Look Around\n2. Yell for Help\n3. Try to find a way out...\n");
         int questionResp = sc.nextInt();
         if(questionResp==1){
-
+            System.out.println("You look around but there is nothing useful,\n" +
+                    "it seems like you were in a simulation or maybe some kind of\n" +
+                    "arena? How strange...");
+            portal_start();
         }else if(questionResp==2){
-
+            System.out.println("You yell for help, but nobody hears you\n" +
+                    "there is nobody around you either, were they even real?...");
+            portal_start();
         }else if(questionResp==3){
-
+            System.out.println("It appears, there is some kind of hologram, which was\n" +
+                    "the race you were in. It is glitching in and out. But in reality,\n" +
+                    "you weren't racing at all and you've been in a simulation\n" +
+                    "the whole time...");
+            portal_start();
         }else{
             System.out.println("That is not a valid responce, please enter one...\n");
             hiddenMethod2();
         }
+    }
+    static void portal_start(){
+
     }
 
 }
